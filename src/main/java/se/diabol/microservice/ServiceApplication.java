73b0b2e0e576d2d27e6b017d939d9e9cb380ab07
Patrik Boström <patbos@patbos.com>
2014-06-13 18:20:53 +0200

    Ahh push it!

---
+++ src/main/java/se/diabol/microservice/ServiceApplication.java
@@ -0,0 +1,27 @@
+package se.diabol.microservice;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class ServiceApplication extends Application<ServiceConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+            new ServiceApplication().run(args);
+        }
+
+
+    @Override
+    public void initialize(Bootstrap<ServiceConfiguration> serviceConfigurationBootstrap) {
+
+    }
+
+    @Override
+    public void run(ServiceConfiguration configuration, Environment environment) throws Exception {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+            );
+            environment.jersey().register(resource);
+    }
+}
