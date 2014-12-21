0168a1ef3778677d21f0d9e626db2bafe0bdf23d
Marcus Philip <marcus@diabol.se>
2014-12-21 21:31:52 +0100

    Fix Adaptions for heroku

--- src/main/java/se/diabol/microservice/ServiceConfiguration.java
+++ src/main/java/se/diabol/microservice/ServiceConfiguration.java
@@ -1,7 +1,9 @@
 package se.diabol.microservice;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.dropwizard.Configuration;
+
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class ServiceConfiguration extends Configuration {
@@ -11,6 +13,8 @@ public class ServiceConfiguration extends Configuration {
 
     @NotEmpty
     private String defaultName = "Stranger";
+
+    //private SimpleServerFactory server = new SimpleServerFactory();
 
     @JsonProperty
     public String getTemplate() {
@@ -31,5 +35,13 @@ public class ServiceConfiguration extends Configuration {
     public void setDefaultName(String name) {
         this.defaultName = name;
     }
+
+//    public SimpleServerFactory getServer() {
+//        return server;
+//    }
+//
+//    public void setServer(SimpleServerFactory server) {
+//        this.server = server;
+//    }
 
 }
