59d8f2b3fea0a7850bef3c0efe3465f001ec6970
Andreas Rehn <mrfatstrat@gmail.com>
2016-12-01 14:49:33 +0100

    fixes in release script

--- release.sh
+++ release.sh
@@ -5,8 +5,8 @@ echo "Running release script..."
 echo "another line..." >> dummy.txt
 git config user.name "travis"
 git config user.email "travis@diabol.se"
-ENCRYPTED_KEY="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY="\$encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV="\$encrypted_${ENCRYPTION_LABEL}_iv"
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis_deploy_key.enc -out travis_deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
