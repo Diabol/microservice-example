9398771493be285517695a1434e3a32eecbabb55
Andreas Rehn <mrfatstrat@gmail.com>
2016-12-01 15:09:20 +0100

    fixes in release script

--- release.sh
+++ release.sh
@@ -5,12 +5,14 @@ echo "Running release script..."
 echo "another line..." >> dummy.txt
 git config user.name "travis"
 git config user.email "travis@diabol.se"
+remote=`git config remote.origin.url | sed -n 's/https:\/\/\(.*\)/git@\1/p'`
+git remote set-url origin $remote
 ENCRYPTED_KEY="\$encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV="\$encrypted_${ENCRYPTION_LABEL}_iv"
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis_deploy_key.enc -out travis_deploy_key -d
-chmod 600 deploy_key
+openssl aes-256-cbc -K $encrypted_8ded143b9563_key -iv $encrypted_8ded143b9563_iv -in travis_deploy_key.enc -out travis_deploy_key -d
+chmod 600 travis_deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add travis_deploy_key
 git add dummy.txt
 git commit -m "added line to dummy.txt"
 git push
