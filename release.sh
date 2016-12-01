cc68c67572de128e7c3bde4735ac230d73dc70ee
Andreas Rehn <mrfatstrat@gmail.com>
2016-12-01 20:24:40 +0100

    fixes o release sript

--- release.sh
+++ release.sh
@@ -3,6 +3,12 @@ set -e
 set -x
 echo "Running release script..."
 
+# prepare the ssh key
+openssl aes-256-cbc -K $encrypted_8ded143b9563_key -iv $encrypted_8ded143b9563_iv -in travis_deploy_key.enc -out travis_deploy_key -d
+chmod 600 travis_deploy_key
+eval `ssh-agent -s`
+ssh-add travis_deploy_key
+
 # prepare the repo
 git config user.name "travis"
 git config user.email "travis@diabol.se"
@@ -11,10 +17,6 @@ remote=`git config remote.origin.url | sed -n 's/https:\/\/github.com\/\(.*\)/gi
 git remote remove origin
 git remote add origin $remote
 git fetch
-openssl aes-256-cbc -K $encrypted_8ded143b9563_key -iv $encrypted_8ded143b9563_iv -in travis_deploy_key.enc -out travis_deploy_key -d
-chmod 600 travis_deploy_key
-eval `ssh-agent -s`
-ssh-add travis_deploy_key
 
 # do a change
 git checkout origin/master
