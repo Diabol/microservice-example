a0c021c5d6e1ce3639c983c3f3c2234d85db0219
Andreas Rehn <mrfatstrat@gmail.com>
2016-12-01 20:16:00 +0100

    fixes o release sript

--- release.sh
+++ release.sh
@@ -10,6 +10,7 @@ git config --global push.default simple
 remote=`git config remote.origin.url | sed -n 's/https:\/\/github.com\/\(.*\)/git@github.com:\1/p'`
 git remote remove origin
 git remote add origin $remote
+git fetch
 openssl aes-256-cbc -K $encrypted_8ded143b9563_key -iv $encrypted_8ded143b9563_iv -in travis_deploy_key.enc -out travis_deploy_key -d
 chmod 600 travis_deploy_key
 eval `ssh-agent -s`
