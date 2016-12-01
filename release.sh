9d4f71da68bcd78175070dd920ab712def847a71
Andreas Rehn <mrfatstrat@gmail.com>
2016-12-01 19:57:33 +0100

    fixes o release sript [skip ci]

--- release.sh
+++ release.sh
@@ -2,19 +2,24 @@
 set -e
 set -x
 echo "Running release script..."
-git checkout master
-echo "another line..." >> dummy.txt
+
+# prepare the repo
 git config user.name "travis"
 git config user.email "travis@diabol.se"
 git config --global push.default simple
 remote=`git config remote.origin.url | sed -n 's/https:\/\/github.com\/\(.*\)/git@github.com:\1/p'`
-git remote set-url origin $remote
+git remote remove origin
+git remote add origin $remote
 ENCRYPTED_KEY="\$encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV="\$encrypted_${ENCRYPTION_LABEL}_iv"
 openssl aes-256-cbc -K $encrypted_8ded143b9563_key -iv $encrypted_8ded143b9563_iv -in travis_deploy_key.enc -out travis_deploy_key -d
 chmod 600 travis_deploy_key
 eval `ssh-agent -s`
 ssh-add travis_deploy_key
+
+# do a change
+git checkout origin/master
+echo "another line..." >> dummy.txt
 git add dummy.txt
 git commit -m "added line to dummy.txt"
 git push
